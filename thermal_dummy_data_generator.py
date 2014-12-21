f98d7740e2bf588c98a138b37ee07923d1d868c2
Juampe Lopez <juampe.lopez@gmail.com>
2014-12-21 22:13:07 +0100

    Migrated to numpy, much simpler code.

--- thermal_dummy_data_generator.py
+++ thermal_dummy_data_generator.py
@@ -1,38 +1,25 @@
 #!/usr/bin/python
 
-import random
 import thermal_config as tcfg
+import numpy as np
 import csv
 import time
 
 millis_at_start = int(round(time.time() * 1000))
 
-random_temp_2d_list = []
-
-tmin = tcfg.temperature['tmax']
-tmax = tcfg.temperature['tmin']
-
-for i in range(tcfg.fov['vertical']):
-    pixels = []
-    for j in range(tcfg.fov['horizontal']):
-        t = random.randint(tcfg.temperature['tmin'], tcfg.temperature['tmax'])
-        if t > tmax:
-            tmax = t
-        if t < tmin:
-            tmin = t
-        pixels.append(t)
-    random_temp_2d_list.append(pixels)
+random_temp_2d_matrix = np.random.randint(tcfg.temperature['tmax'],
+                                          size=(tcfg.fov['vertical'],
+                                                tcfg.fov['horizontal']))
 
 with open(tcfg.files['csv'], 'wb') as f:
     writer = csv.writer(f)
-    writer.writerows(random_temp_2d_list)
+    writer.writerows(random_temp_2d_matrix)
 
 millis_now = int(round(time.time() * 1000))
 
 print 'Generated a %dx%d measures array ' \
-      'between %d and %d (min=%d, max=%d), ' \
+      'between %d and %d, ' \
       'took %d ms' \
       % (tcfg.fov['horizontal'], tcfg.fov['vertical'],
          tcfg.temperature['tmin'], tcfg.temperature['tmax'],
-         tmin, tmax,
          millis_now - millis_at_start)
